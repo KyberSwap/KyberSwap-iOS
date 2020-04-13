3d981bac7c328a05beab474a091d392ea07bc190
Quan <quan.ta@kyber.network>
2020-04-13 17:16:15 +0700

    - update display -- instead of 0

--- KyberNetwork/KyberNetwork/Screens/Tabbars/LimitOrder/Controllers/SelectMarket/Cell/KNMarketTableViewCell.swift
+++ KyberNetwork/KyberNetwork/Screens/Tabbars/LimitOrder/Controllers/SelectMarket/Cell/KNMarketTableViewCell.swift
@@ -79,9 +79,17 @@ class KNMarketTableViewCell: UITableViewCell {
   func updateViewModel(_ viewModel: KNMarketCellViewModel) {
     self.viewModel = viewModel
     self.pairNameLabel.text = viewModel.pairName
-    self.priceLabel.text = viewModel.price
-    self.volumeLabel.text = viewModel.volume
-    self.changeButton.setAttributedTitle(viewModel.change24h, for: .normal)
+    self.priceLabel.text = viewModel.price.doubleValue == 0 ? "--" : viewModel.price
+    self.volumeLabel.text = viewModel.volume.doubleValue == 0 ? "--" : viewModel.volume
+    if viewModel.price.doubleValue == 0 {
+      let zeroAttributes: [NSAttributedStringKey: Any] = [
+        NSAttributedStringKey.font: UIFont.Kyber.medium(with: 12),
+        NSAttributedStringKey.foregroundColor: UIColor.Kyber.grayChateau,
+      ]
+      self.changeButton.setAttributedTitle(NSAttributedString(string: "--", attributes: zeroAttributes), for: .normal)
+    } else {
+      self.changeButton.setAttributedTitle(viewModel.change24h, for: .normal)
+    }
     let favImg = viewModel.isFav ? UIImage(named: "selected_fav_icon") : UIImage(named: "unselected_fav_icon")
     self.favoriteButton.setImage(favImg, for: .normal)
   }
