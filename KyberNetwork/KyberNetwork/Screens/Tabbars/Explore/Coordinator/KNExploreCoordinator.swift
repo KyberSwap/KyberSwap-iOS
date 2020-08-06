c168697c2806c09e7b90ef48cdc942c891492b4b
Quan <quan.ta@kyber.network>
2020-08-06 16:23:03 +0700

    - validate link has http:// or https://

--- KyberNetwork/KyberNetwork/Screens/Tabbars/Explore/Coordinator/KNExploreCoordinator.swift
+++ KyberNetwork/KyberNetwork/Screens/Tabbars/Explore/Coordinator/KNExploreCoordinator.swift
@@ -108,6 +108,9 @@ extension KNExploreCoordinator: KNExploreViewControllerDelegate {
     case .openLogin:
       self.profileCoordinator.start()
     case .openBannerLink(let link):
+      guard link.contains("http:'//") || link.contains("https://") else {
+        return
+      }
       self.rootViewController.openSafari(with: link)
     case .navigateSwap:
       self.rootViewController.tabBarController?.selectedIndex = 1
