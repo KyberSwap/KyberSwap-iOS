a45735948c40ad8ee6b6006f3137e409abf96583
Quan <quan.ta@kyber.network>
2020-08-12 15:54:07 +0700

    - refactor code

--- KyberNetwork/KyberNetwork/Coordinators/TransactionCoordinator/KNTransactionCoordinator.swift
+++ KyberNetwork/KyberNetwork/Coordinators/TransactionCoordinator/KNTransactionCoordinator.swift
@@ -319,13 +319,6 @@ extension KNTransactionCoordinator {
     if transactions.isEmpty { return }
     self.handleReceiveEtherOrToken(transactions)
     self.transactionStorage.add(transactions)
-    if self.transactionStorage.transferNonePendingObjects.count > 1000 {
-      let sortedTxObjects = self.transactionStorage.transferNonePendingObjects.sorted { (left, right) -> Bool in
-        return left.date > right.date
-      }
-      let suffixedObject = sortedTxObjects.suffix(from: 1000)
-      self.transactionStorage.delete(Array(suffixedObject))
-    }
     KNNotificationUtil.postNotification(for: kTokenTransactionListDidUpdateNotificationKey)
     var tokenObjects: [TokenObject] = []
     let savedTokens: [TokenObject] = self.tokenStorage.tokens
