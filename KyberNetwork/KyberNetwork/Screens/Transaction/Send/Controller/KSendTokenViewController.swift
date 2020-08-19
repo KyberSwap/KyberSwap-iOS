6c4daf531be98822438b1d2d214385057bb1bf4e
Quan <quan.ta@kyber.network>
2020-08-19 11:30:44 +0700

    - change condition to auto scroll

--- KyberNetwork/KyberNetwork/Screens/Transaction/Send/Controller/KSendTokenViewController.swift
+++ KyberNetwork/KyberNetwork/Screens/Transaction/Send/Controller/KSendTokenViewController.swift
@@ -730,7 +730,7 @@ extension KSendTokenViewController: KAdvancedSettingsViewDelegate {
           self.updateAdvancedSettingsView()
           self.view.layoutIfNeeded()
         }, completion: { _ in
-          if self.advancedSettingsView.isExpanded && self.scrollContainerView.contentSize.height > self.scrollContainerView.bounds.size.height + 100 {
+          if self.advancedSettingsView.isExpanded && self.scrollContainerView.bounds.height < 454 {
             let bottomOffset = CGPoint(
               x: 0,
               y: 128
